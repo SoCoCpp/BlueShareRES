fad9bde6d97626535b9de0c83e110cf1bf6e6c4d
SoCo <soco@socosoftware.com>
2017-03-01 20:58:01 -0600

    Revised columns. Added min/max for paging. Added output for 5 pages.

--- format.js
+++ format.js
@@ -36,41 +36,47 @@ function init()
 function updateData(data)
 {
 	var i;
-	var html = "<table><tr><th>Name</th><th>Tag</th><th>Link</th></tr>";
+	var html = "<table><tr><th>&#35;</th><th>votes</th><th>Name</th><th>Tag</th><th>Link</th></tr>";
 	var cnt = 0;
+	var cntMin = 0;
+	var cntMax = 0; // 10540 total, 5 pages: 0, 2108, 4216, 6324, 8432
 	var excols = "";
 	for (name in data)
 	{
-		var color = "";
-		var votes = "";
-		var tag = "&nbsp;";
-		var link = "&nbsp;";
-		
-		if (data[name].hasOwnProperty("color"))
-			color = " style='color:" + data[name]["color"] + ";'"
-		if (data[name].hasOwnProperty("votes"))
-			votes = "(" + data[name]["votes"] + ") ";
-		if (data[name].hasOwnProperty("tag"))
-			tag = " [" + data[name]["tag"] + "]"
-		if (data[name].hasOwnProperty("link"))
+		if (cntMin == cntMax || (cnt >= cntMin && cnt < cntMax))
 		{
-			link = data[name]["link"];
-			var linkParts = link.split("/");
-			var linkTitle = "";
-			var linkSub = "";
-			if (linkParts.length >= 7 && link.startsWith("http"))
-				linkTitle = "(/" + linkParts[3] + "/" + linkParts[4] + ") " + linkParts[7];
-			else linkTitle = "(link)";
-			link = "<a href='" + link + "'>" + linkTitle + "</a>";
+			var color = "";
+			var votes = "&nbsp;";
+			var tag = "&nbsp;";
+			var link = "&nbsp;";
+			
+			if (data[name].hasOwnProperty("color"))
+				color = " style='color:" + data[name]["color"] + ";'"
+			if (data[name].hasOwnProperty("votes"))
+				votes = data[name]["votes"];
+			if (data[name].hasOwnProperty("tag"))
+				tag = data[name]["tag"];
+			if (data[name].hasOwnProperty("link"))
+			{
+				link = data[name]["link"];
+				var linkParts = link.split("/");
+				var linkTitle = "";
+				var linkSub = "";
+				if (linkParts.length >= 7 && link.startsWith("http"))
+					linkTitle = "(/" + linkParts[3] + "/" + linkParts[4] + ") " + linkParts[7];
+				else linkTitle = "(link)";
+				link = "<a href='" + link + "'>" + linkTitle + "</a>";
+			}
+			
+			html += "<tr" + color + ">";
+			html += "<td>" + cnt + "</td><td>" + votes + "</td><td>" + name + "</td><td>" + tag + "</td><td>" + link + "</td>";
+			html += "</tr>";
 		}
 		
-		html += "<tr" + color + ">";
-		html += "<td>" + votes + name + "</td><td>" + tag + "</td><td>" + link + "</td>";
-		html += "</tr>";
 		cnt++;
 	 
 	}
 	html += "</table>";
-	html += "<br/>Count: " + cnt + "<br/>";
+	//html += "<br/>Count: " + cnt + "<br/>";
 	document.getElementById("out").innerHTML = html;
 }
